d1eda33b02aee90286fb95083b5acaa271d45ad4
Anya <anyutastefurak@gmail.com>
2024-10-01 08:30:46 -0700

    first master commit

---
+++ 52222222222/52222222222.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+
+int main()
+{
+    double xp, xk, x, dx, eps, s = 0;
+    int n = 0;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+    cout << fixed;
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "|" << setw(10) << "x" << " |"
+        << setw(15) << "cos(x)" << " |"
+        << setw(15) << "S" << " |"
+        << setw(10) << "n" << " |"
+        << endl;
+    cout << "-----------------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        s = S(x, eps, n, s);
+        cout << "|" << setw(10) << setprecision(2) << x << " |"
+            << setw(15) << setprecision(10) << cos(x) << " |"
+            << setw(15) << setprecision(10) << s << " |"
+            << setw(10) << n << " |"
+            << endl;
+        x += dx;
+    }
+    cout << "-----------------------------------------------------------" << endl;
+    return 0;
+}
+
+double S(const double x, const double eps, int& n, double s)
+{
+    n = 0;
+    double a = 1;
+    s = a;
+    do {
+        n++;
+        a = A(x, n, a);
+        s += a;
+    } while (abs(a) >= eps);
+    return s;
+}
+
+double A(const double x, const int n, double a)
+{
+    double R = -x * x / ((2 * n - 1) * (2 * n));
+    a *= R;
+    return a;
+}
---
+++ UnitTest5222222/UnitTest5222222.cpp
@@ -0,0 +1,41 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <cmath> 
+#include "../52222222222/52222222222.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest52
+{
+	TEST_CLASS(UnitTest52)
+	{
+	public:
+
+		TEST_METHOD(TestAFunction)
+		{
+		
+			double x = 1.0;  
+			int n = 1;
+			double a = 1.0;
+
+		
+			double expected = -x * x / 2;
+
+			// Перевірка, що результат наближений до очікуваного
+			Assert::IsTrue(fabs(A(x, n, a) - expected) < 1e-10, L"Test A failed");
+		}
+
+		TEST_METHOD(TestSFunction)
+		{
+			
+			double x = 1.0;  
+			double eps = 1e-10;
+			int n = 0;
+			double s = 0;
+
+			double expected = cos(x);
+
+			Assert::IsTrue(fabs(S(x, eps, n, s) - expected) < eps, L"Test S failed");
+		}
+	};
+}
